a7b6ccc8ffce5945ca64e9d3c52b2db69728978d
Gopalakrishnan <gopalakrishnan122005@gmail.com>
2026-05-15 12:14:24 +0530

    Delete product-supplier-service directory

--- product-supplier-service/src/main/java/com/cts/repository/SupplierRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cts.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.entity.Supplier;
-public interface SupplierRepository extends JpaRepository<Supplier, Long> {
-}
